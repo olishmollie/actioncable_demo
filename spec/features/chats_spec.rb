4b183cd8ddb9ce25f608295fccdf318550cfa34f
AJ Bond <olishmollie@gmail.com>
2017-04-30 15:57:36 -0500

    Chats spec checks for repeat chats

--- spec/features/chats_spec.rb
+++ spec/features/chats_spec.rb
@@ -13,14 +13,14 @@ RSpec.feature "Chats", type: :feature, js: true do
       create_guest 'Picard'
       chat 'Energize'
     end
-    expect(page).to have_content('Picard: Energize')
+    expect(page).to have_content 'Picard: Energize', count: 1
   end
 
   scenario "user can post message in chat box" do
     create_guest 'Picard'
     chat 'Energize'
     expect(chat_input).to_not have_content 'Energize'
-    expect(page).to have_content 'Picard: Energize'
+    expect(page).to have_content 'Picard: Energize', count: 1
   end
 
   scenario "another user can see any messages posted by guests" do
@@ -30,7 +30,7 @@ RSpec.feature "Chats", type: :feature, js: true do
       create_guest 'Riker'
       chat 'Red alert!'
     end
-    expect(page).to have_content 'Riker: Red alert!'
+    expect(page).to have_content 'Riker: Red alert!', count: 1
   end
 
 end
